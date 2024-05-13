1cf7237b9e942d4b02760a054f7740bb50d88d41
RAAVSOM <121207361+RAAVSOM@users.noreply.github.com>
2024-05-13 16:30:49 -0500

    1.0

---
+++ src/main/java/com/app/InBestBackend/InBestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.InBestBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InBestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InBestBackendApplication.class, args);
+	}
+
+}
